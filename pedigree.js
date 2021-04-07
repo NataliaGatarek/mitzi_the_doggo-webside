cbe7e8de504afb246c75190e10470708c236d513
Your Name <you@example.com>
2021-04-07 17:00:27 +0200

    3rd commit

---
+++ pedigree.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundImage = "url(orange-texture.jpg)";
+document.getElementById("myDiv").setAttribute("align", "center");
+document.getElementById("myDiv").style.backgroundImage = "url(wall.jpg)";
+document.getElementById("myDiv").style.marginTop = "20px";
+document.getElementById("myDiv").style.border = "thick solid black";
+document.getElementById("myDiv").style.marginBottom = "50px";
+function goBack() {
+  window.history.back();
+}
+document.getElementById("button").style.marginBottom = "50px";
+document.getElementById("button").style.marginLeft = "50%";
