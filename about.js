c1f6209905e2384ed8b2fa39827a18237f99f0df
-- <nova.nathalia@gmail.com>
2021-04-08 17:08:29 +0200

    4th commit

--- about.js
+++ about.js
@@ -1,3 +1,4 @@
 document.getElementById("myButton").onclick = function () {
         location.href = "pedigree.html";
-    };
+};
+document.getElementById("about").style.backgroundImage = "url(orange-texture.jpg)";
---
+++ blog.js
@@ -0,0 +1,5 @@
+
+document.getElementById("card1").style.backgroundImage = "url(wall.jpg)";
+document.getElementById("card2").style.backgroundImage = "url(wall.jpg)";
+document.getElementById("card3").style.backgroundImage = "url(wall.jpg)";
+document.getElementById("bg").style.backgroundImage = "url(orange-texture.jpg)";
