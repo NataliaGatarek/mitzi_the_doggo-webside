6bced1d8b0a9278d9287e5244eadf0999cf33295
-- <nova.nathalia@gmail.com>
2021-04-09 12:01:04 +0200

    4.2 commit

--- about.js
+++ about.js
@@ -2,3 +2,4 @@ document.getElementById("myButton").onclick = function () {
         location.href = "pedigree.html";
 };
 document.getElementById("about").style.backgroundImage = "url(orange-texture.jpg)";
+document.getElementById("aboutbg").style.backgroundColor = "grey";
--- pedigree.js
+++ pedigree.js
@@ -1,6 +1,6 @@
-document.body.style.backgroundImage = "url(orange-texture.jpg)";
+document.body.style.backgroundImage = "url(wall4.jpg)";
 document.getElementById("myDiv").setAttribute("align", "center");
-document.getElementById("myDiv").style.backgroundImage = "url(wall.jpg)";
+document.getElementById("myDiv").style.backgroundImage = "url(orange-texture.jpg)";
 document.getElementById("myDiv").style.marginTop = "20px";
 document.getElementById("myDiv").style.border = "thick solid black";
 document.getElementById("myDiv").style.marginBottom = "50px";
@@ -9,3 +9,6 @@ function goBack() {
 }
 document.getElementById("button").style.marginBottom = "50px";
 document.getElementById("button").style.marginLeft = "43%";
+document.getElementById("myDiv").style.tableLayout = "fixed";
+document.getElementById("myDiv").style.textAlign = "center";
+document.getElementById("myDiv").style.fontFamily = "Courgette,sans-serif";
