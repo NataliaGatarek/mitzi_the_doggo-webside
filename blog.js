620a186d316ec9a79eb06ec9ec403a22bb2720e4
-- <nova.nathalia@gmail.com>
2021-04-11 17:44:49 +0200

    6.1 commit

--- blog.js
+++ blog.js
@@ -3,6 +3,23 @@ document.getElementById("bg").style.backgroundImage = "url(orange-texture.jpg)";
 document.getElementById("card3").style.backgroundColor = "grey";
 document.getElementById("card2").style.backgroundColor = "grey";
 document.getElementById("card1").style.backgroundColor = "grey";
+var para = document.createElement("p");
+var node = document.createTextNode("Galery with the pictures of Mitzi and Zlata");
+para.appendChild(node);
+var element = document.getElementById("galery");
+element.appendChild(para);
+
+document.getElementById("body2").style.backgroundImage = "url(orange-texture.jpg)";
+document.getElementById("galery").style.fontSize = "27px";
+document.getElementById("galery").style.fontWeight = "600";
+var para = document.createElement("p");
+var node = document.createTextNode("Blog about adventures of Mitzi and Zlata");
+para.appendChild(node);
+var element = document.getElementById("blog1");
+element.appendChild(para);
+document.getElementById("blog1").style.fontSize = "27px";
+document.getElementById("blog1").style.fontWeight = "600";
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -23,90 +40,12 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-var para = document.createElement("p");
-var node = document.createTextNode("Our vacation in Kleve was very nice. We had so much fun there.");
-para.appendChild(node);
-var element = document.getElementById("div1");
-element.appendChild(para);
 
-// Get the modal
-var modal = document.getElementById("myModal2");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn2");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close2")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-var para = document.createElement("p");
-var node = document.createTextNode("Winter in Berlin this year was very long and cold.");
-para.appendChild(node);
-var element = document.getElementById("div2");
-element.appendChild(para);
-// Get the modal
-var modal = document.getElementById("myModal2");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn3");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close3")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-var para = document.createElement("p");
-var node = document.createTextNode("The New Years Day we spent at the Sea side, in Heringsdorf");
-para.appendChild(node);
-var element = document.getElementById("div3");
-element.appendChild(para);
-
-var para = document.createElement("p");
-var node = document.createTextNode("Galery with the pictures of Mitzi and Zlata");
-para.appendChild(node);
-var element = document.getElementById("galery");
-element.appendChild(para);
-document.getElementById("body2").style.backgroundImage = "url(orange-texture.jpg)";
-document.getElementById("galery").style.fontSize = "27px";
-document.getElementById("galery").style.fontWeight = "600";
-var para = document.createElement("p");
-var node = document.createTextNode("Blog about adventures of Mitzi and Zlata");
-para.appendChild(node);
-var element = document.getElementById("blog1");
-element.appendChild(para);
-document.getElementById("blog1").style.fontSize = "27px";
-document.getElementById("blog1").style.fontWeight = "600";
