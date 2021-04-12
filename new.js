a1920476afb7b29380bc15664bed364a212c1089
-- <nova.nathalia@gmail.com>
2021-04-12 15:16:50 +0200

    7.1 commit

--- new.js
+++ new.js
@@ -1,11 +1,20 @@
-var video = document.getElementById("myVideo");
+ var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
-function myFunction() {
+function myFunctionOne() {
   if (video.paused) {
     video.play();
     btn.innerHTML = "Pause";
   } else {
     video.pause();
     btn.innerHTML = "Play";
+  }
+}
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
